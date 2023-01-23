7cd65c37d11681835c595cfab4a93a945a0b31d3
Sanu Shilshad <sanu.shilshad@acelrtech.com>
2023-01-23 12:54:38 +0000

    feat: :sparkles: optimize the xml batch creation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use xmltree::{Element, XMLNode};
+use xmltree::{Element};
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read};
 fn main(){
     // read and remove special characters
     let mut file = File::open("dict.xml").unwrap();
@@ -9,35 +9,17 @@ fn main(){
     let xml_string = xml_string.replace("&#4;", "");
 
     // parse the xml file
-    let mut root = Element::parse(xml_string.as_bytes()).unwrap();
-    let children = root.children.clone();
-    
-
-    // calculating slice index based on the batch size and children node length
+    let root = Element::parse(xml_string.as_bytes()).unwrap();
+    let children = root.children;
     let batch_size = 1;
-    let children_len= children.len() as i32;
-    let mut start_index = 0;
-    let mut end_index = batch_size as usize;
-    let limit= (children_len as f64/ batch_size as f64).ceil();
-
-    let mut vec: Vec<XMLNode> = Vec::new();
-
-    // create xm by batch
-    for i in 1..=limit as i32 {
-        println!("Start: {}, End: {}", start_index, end_index);
-        root.children.clear();
-        let sliced = &children[start_index..end_index];
-        root.children = sliced.to_vec();
-        vec.push(XMLNode::Element(root.clone()));
-
-        start_index = end_index;
-        end_index = ((i) + batch_size) as usize;
-
+    for (_i, chunk)  in children.chunks(batch_size).enumerate()  {
+        let mut root = Element::new("LISTOFLEDGERS");
+        root.children = chunk.to_vec();
+        println!("{:?}",root);  
 
     }
+}
 
     // println!("{:#?}", vec[1]);
 
-
-}
 
